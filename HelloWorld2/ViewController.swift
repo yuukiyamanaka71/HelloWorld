e4aa1aec95ff024377bd2ff5ba8bfa4b0b1cd653
yuukiyamanaka71 <y.yamanaka71@gmail.com>
2018-11-02 17:56:20 +0900

    HelloWorldのラベルを追加した

---
+++ HelloWorld2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld2
+//
+//  Created by 山中祐樹 on 2018/11/02.
+//  Copyright © 2018 山中祐樹. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
